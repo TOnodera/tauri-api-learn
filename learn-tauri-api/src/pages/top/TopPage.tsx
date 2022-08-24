4e96181298c24dd4691c0ecbac0e9ea4bd7ec02c
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-08-24 18:56:22 +0900

    appはこんなもんだろ

--- learn-tauri-api/src/pages/top/TopPage.tsx
+++ learn-tauri-api/src/pages/top/TopPage.tsx
@@ -1,7 +1,11 @@
 export default function Top() {
   return (
     <>
-      <h2>このページについて</h2>
+      <h2 className="mb-10">このページについて</h2>
+      <p>
+        このページはTauri
+        Apiの学習用に公開されているAPIドキュメントの内容を試しに実装してみたものです。
+      </p>
     </>
   );
 }
