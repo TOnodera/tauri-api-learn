d15c53c2640544e6dc25fa1127ed3cb0feb546e7
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-08-24 18:56:18 +0900

    appはこんなもんだろ

--- learn-tauri-api/src/pages/app/AppPage.tsx
+++ learn-tauri-api/src/pages/app/AppPage.tsx
@@ -1,5 +1,13 @@
 import { getName as getTauriAppName } from "@tauri-apps/api/app";
 import { useEffect, useState } from "react";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  TextField,
+} from "@mui/material";
+import { textTransform } from "@mui/system";
 export default function AppPage() {
   const [appName, setAppName] = useState("");
   useEffect(() => {
@@ -12,8 +20,16 @@ export default function AppPage() {
   }, [appName]);
   return (
     <>
-      <h2>app</h2>
-      <p>AppName: {appName}</p>
+      <h2 className="mb-10">appモジュール</h2>
+      <Card>
+        <CardContent>
+          <h2 className="mb-10">getName() : Promise</h2>
+          <p className="mb-10">アプリケーション名を取得します。</p>
+          <Button style={{ textTransform: "none" }} variant="contained">
+            getName()
+          </Button>
+        </CardContent>
+      </Card>
     </>
   );
 }
